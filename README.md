02eef133a70e0b1854673e678548e04c9a1344c6
chanwoo99 <ixora990919@gmail.com>
2021-10-09 15:47:00 +0900

    dd

---
+++ README.md
@@ -0,0 +1,15 @@
+- 👋 Hi, I’m @chanwoo99
+- 👀 I’m interested in ...
+- 🌱 I’m currently learning ...
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me ...
+ddd
+
+testse
+# Blog posts
+[DD](https://chanwooo.tistory.com/5) - Oct 09, 2021<br>
+[공부할 때 참고한 자료들 정리](https://chanwooo.tistory.com/4) - Oct 05, 2021<br>
+[비행체 자율비행 관련 용어 정리](https://chanwooo.tistory.com/3) - Sep 30, 2021<br>
+ddd
+sd
+sd
---
+++ run.py
@@ -0,0 +1,59 @@
+import feedparser, datetime
+
+
+feed = feedparser.parse("https://chanwooo.tistory.com/rss")
+
+parsed_data = ""
+
+for i in feed['entries']:
+    dt = datetime.datetime.strptime(i['published'], "%a, %d %b %Y %H:%M:%S %z").strftime("%b %d, %Y")
+    parsed_data += f"[{i['title']}]({i['link']}) - {dt}<br>\n"
+    print(i['link'], i['title'])
+
+    
+
+import urllib.request
+
+
+url="https://raw.githubusercontent.com/chanwoo99/chanwoo99/main/README.md"
+
+response = urllib.request.urlopen(url)
+
+text = response.read().decode("utf-8")
+
+
+f = open("README.md", mode="w", encoding="utf-8")
+f.write(text)
+f.close()
+
+f = open("README.md", mode="r", encoding="utf-8")
+data=f.readlines()
+f.close()
+
+
+result=""
+flag=0
+for i in data:
+    if i == "<!-- BLOG-POST-LIST:START -->\n":
+        flag = 1
+        result += parsed_data
+        continue
+    elif i == "<!-- BLOG-POST-LIST:END -->\n":
+        flag = 0
+        continue
+
+    if flag == 0:
+        result +=i
+ 
+
+f = open("README.md", mode="w", encoding="utf-8")
+f.write(result)
+f.close()
+
+
+
+
+        
+
+
+
